d5bb2775da54e208e56457b9de21149dec61a06b
spacegoing <spacegoing@gmail.com>
2017-04-29 18:48:11 +0800

    [bugfix] np.random.choice return repetitive results, change to random.sample

--- sogou_spider/sogou_helpers.py
+++ sogou_spider/sogou_helpers.py
@@ -4,15 +4,14 @@ __author__ = 'spacegoing'
 import urllib.parse as ps
 from itertools import combinations
 import csv
-import numpy as np
-from numpy.random import choice
+from random import sample
 
 with open('./hs300_names.csv', newline='') as f:
     reader = csv.reader(f)
-    name_list = np.array([r[0] for r in reader])
+    name_list = [r[0] for r in reader]
 
 # Randomly choose 50 names
-rand_name_list = choice(name_list, 50)
+rand_name_list = sample(name_list,50)
 
 # Encode Chinese to urlstring
 encode_list = [ps.quote('"%s"' % n) for n in rand_name_list]
@@ -26,3 +25,4 @@ combo_names = list(combinations(encode_list, 2))
 url_template = 'https://www.sogou.com/web?query=%s+AND+%s+AND+%s'
 coop_url_list = [url_template % (c[0], c[1], coop) for c in combo_names]
 compete_url_list = [url_template % (c[0], c[1], compete) for c in combo_names]
+
